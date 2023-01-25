677b72ffd28d522dcdde2b7c7349a77933cf23fe
ofb-bbx <matteo9o@outlook.fr>
2023-01-25 19:59:21 +0100

    fixed error with getstring

--- commands/8ball.js
+++ commands/8ball.js
@@ -5,10 +5,11 @@ module.exports = {
         .setName('8ball')
         .setDescription('Réponds à votre question.')
         .addStringOption(option =>
-            option.setName('Question')
-                .setDescription('La question que vous voulez posez.')),
+            option.setName('question')
+                .setDescription('La question que vous voulez posez.')
+                .setRequired(true)),
     async execute(interaction) {
-        let question = interaction.getString('Question');
+        let question = interaction.option.getString('question');
         console.log(question);
     }
 }
