c0c4647ce36c8341b0ecd705abe1f1859c6b93aa
ofb-bbx <matteo9o@outlook.fr>
2023-01-25 19:56:09 +0100

    test version of 8ball

--- commands/8ball.js
+++ commands/8ball.js
@@ -1,1 +1,14 @@
 const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('8ball')
+        .setDescription('Réponds à votre question.')
+        .addStringOption(option =>
+            option.setName('Question')
+                .setDescription('La question que vous voulez posez.')),
+    async execute(interaction) {
+        let question = interaction.getString('Question');
+        console.log(question);
+    }
+}
