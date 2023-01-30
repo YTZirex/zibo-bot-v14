b5886000bd76880dd33c8bf9b8e06d7e48ccb8bc
ofb-bbx <matteo9o@outlook.fr>
2023-01-30 22:13:21 +0100

    added colors to connected message

--- events/ready.js
+++ events/ready.js
@@ -1,7 +1,10 @@
+const colors = require('colors');
+colors.enable();
+
 module.exports = {
 	name: 'ready',
 	once: true,
 	execute(client) {
-		console.log(`Connecté en tant que ${client.user.tag}`);
+		console.log(`Connecté en tant que ${client.user.username} #${client.user.discriminator}`.green.italic.bold);
 	},
 };
